b260e7f3af9823a9e82baa28e2b27fab8d95b067
Miguel Martín <alu0101209777@ull.edu.es>
2022-06-12 22:06:11 +0100

    comment added to get_available_videos

--- deepcom/apps.py
+++ deepcom/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class DeepcomConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'deepcom'
+    videos_path = 'C:/Users/migue/Documents/U/4to/TFG/Videos'
+    allowed_extensions = ('.mp4')
--- deepcom/services/videos.py
+++ deepcom/services/videos.py
@@ -1,18 +1,39 @@
+import os
+
+from deepcom.apps import DeepcomConfig
+from deepviewcore.Video import Video
 
 
 class VideoService:
-  def __init__(self):
-    pass
-  def get_available_videos():
-    video = {
-      "name": "video2.mp4",
-      "size": "1.0 MB",
-      "date": "01/01/2019",
-      "duration": "00:00:00",
-      "fps": "30",
-      "resolution": "1280x720",
-      "status": "Procesando...",
-    }
+    def __init__(self):
+        pass
 
-    available_videos = [video]
-    return available_videos
+    def validate_video_file(filename):
+        videos_path = DeepcomConfig.videos_path
+        return os.path.isfile(os.path.join(videos_path, filename)) and \
+            filename.endswith(DeepcomConfig.allowed_extensions)
+
+    def get_available_videos():
+        """Gets available videos stats from the videos folder.
+        Each video stats is a dictionary with the following keys:
+         - name: video name
+         - size_in_bytes: video size in bytes
+         - duration_in_seconds: video duration in seconds
+         - fps: video frames per second
+        
+        """
+
+        videos_names = []
+        for filename in os.listdir(DeepcomConfig.videos_path):
+            if VideoService.validate_video_file(filename):
+                videos_names.append(filename)
+
+        videos_stats = []
+        for videopath in [os.path.join(DeepcomConfig.videos_path, videoname) for videoname in videos_names]:
+            video = Video(videopath)
+            current_stats = video.getStats()
+            del current_stats['path']
+            current_stats['name'] = os.path.basename(videopath)
+            current_stats['status'] = 'UNPROCESSED'
+            videos_stats.append(current_stats)
+        return videos_stats

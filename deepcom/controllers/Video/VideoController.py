91b85629c2498a2673b24f943516e8addd229292
Miguel Martín <alu0101209777@ull.edu.es>
2022-06-15 16:41:43 +0100

    check-status action added

--- deepcom/controllers/Video/VideoController.py
+++ deepcom/controllers/Video/VideoController.py
@@ -1,14 +1,20 @@
 import json
 from django.http import HttpRequest, HttpResponse
 
+from .check_status import check_status
 from .process import process_video
 from .stop_processing import stop_processing
 
 
 class VideoController:
-    actions = {
+    post_actions = {
         "stop": stop_processing,
-        "process": process_video
+        "process": process_video,
+        "check_status": check_status
+    }
+
+    get_actions = {
+      "check-status": check_status,
     }
 
     def __init__(self):
@@ -17,9 +23,12 @@ class VideoController:
     def __call__(self, request: HttpRequest) -> HttpResponse:
         if request.method == 'POST':
             response = VideoController.handlePostRequest(request)
-            return HttpResponse(json.dumps(response), content_type='application/json')
+        elif request.method == 'GET':
+            response = VideoController.handleGetRequest(request)
         else:
             return HttpResponse(status=400)
+
+        return HttpResponse(json.dumps(response), content_type='application/json')
 
     def handlePostRequest(request):
         body = VideoController.parseRequestBody(request.body)
@@ -30,7 +39,7 @@ class VideoController:
             }
             return response
 
-        if not body['action'] in VideoController.actions:
+        if not body['action'] in VideoController.post_actions:
             response = {
                 "success": False,
                 "message": f"Action {body['action']} not found"
@@ -49,5 +58,28 @@ class VideoController:
         json_body = json.loads(decoded_body)
         return json_body
 
+    def handleGetRequest(request):
+        action = request.GET.get('action')
+        if not action:
+            response = {
+                "success": False,
+                "message": "No action provided"
+            }
+            return response
+
+        if not action in VideoController.get_actions:
+            response = {
+                "success": False,
+                "message": f"Action {action} not found"
+            }
+            return response
+
+        payload = request.GET.get('payload')
+        response = VideoController.get(action, payload)
+        return response
+
     def post(action, payload):
-        return VideoController.actions[action](payload)
+        return VideoController.post_actions[action](payload)
+
+    def get(action, payload):
+        return VideoController.get_actions[action](payload)
--- deepcom/urls.py
+++ deepcom/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.say_hello),
     path('processed-video/', views.processed_video),
     path('available-videos/', views.available_videos),
-    path('process-video/', views.process_video),
+    # path('process-video/', views.process_video),
     path('check-status/', views.check_status),
     path('video', VideoController())
 ]

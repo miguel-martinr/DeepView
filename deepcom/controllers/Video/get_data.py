66e078f7f6bd24887cc06c1bfac710aeae79b617
Miguel Martín <alu0101209777@ull.edu.es>
2022-06-20 13:34:21 +0100

    data is summarized

--- deepcom/controllers/Video/get_data.py
+++ deepcom/controllers/Video/get_data.py
@@ -4,6 +4,19 @@ from deepcom.apps import DeepcomConfig
 from deepcom.models import VideoModel
 
 
+def summarize_frames(size, frames):
+    """
+    Summarize frames.
+    """
+    result = []
+    for i in range(0, len(frames), size):
+        segment = frames[i:i+size]
+        # print(f"###Segment: {segment}")
+        result.append(sum([len(frame['particles']) for frame in segment]) / len(segment))
+
+    return result
+
+
 def get_data(request: HttpRequest):
     """
     Get data from the video.
@@ -21,8 +34,15 @@ def get_data(request: HttpRequest):
     except KeyError:
         return {
             'success': False,
-            'message': 'No video name provided',
+            'message': 'No params provided',
         }
+
+    params = json.loads(params)
+
+    # Summarize frames
+    response = {
+        'success': True,
+    }
 
     video_path = DeepcomConfig.getVideoPath(video_name)
 
@@ -34,9 +54,14 @@ def get_data(request: HttpRequest):
 
     video_model: VideoModel = VideoModel.objects.get(video_path=video_path)
 
-    response = {"success": True}
-    response["message"] = {
-        "frames": video_model.frames
-    }
+    summarizeFrames = params.get('summarizeFrames')
+    if summarizeFrames:
+        response['message'] = {
+            'frames': summarize_frames(summarizeFrames, video_model.frames),
+        }
+    else:
+        response["message"] = {
+            "frames": video_model.frames
+        }
 
     return response

f76ee7352bf08489556f1cef498cf40e3eab961d
Miguel Martín <alu0101209777@ull.edu.es>
2022-06-30 21:40:09 +0100

    ParameterController added

--- deepcom/controllers/Parameters/ParametersController.py
+++ deepcom/controllers/Parameters/ParametersController.py
@@ -3,25 +3,13 @@ from django.http import HttpRequest, HttpResponse
 
 
 
-from .get_data import get_data
-from .list_available import list_available
-from .check_status import check_status
-from .process import process_video
-from .stop_processing import stop_processing
-from .process_frame import process_frame
-
-
-class VideoController:
+class ParametersController:
     post_actions = {
-        "stop": stop_processing,
-        "process": process_video,
-        "process_frame": process_frame,        
+       
     }
 
     get_actions = {
-        "check-status": check_status,
-        "list-available": list_available,
-        "get-data": get_data,        
+
     }
 
     def __init__(self):
@@ -29,16 +17,16 @@ class VideoController:
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
         if request.method == 'POST':
-            response = VideoController.handlePostRequest(request)
+            response = ParametersController.handlePostRequest(request)
         elif request.method == 'GET':
-            response = VideoController.handleGetRequest(request)
+            response = ParametersController.handleGetRequest(request)
         else:
             return HttpResponse(status=400)
 
         return HttpResponse(json.dumps(response), content_type='application/json')
 
     def handlePostRequest(request):
-        body = VideoController.parseRequestBody(request.body)
+        body = ParametersController.parseRequestBody(request.body)
         if not body['action']:
             response = {
                 "success": False,
@@ -46,7 +34,7 @@ class VideoController:
             }
             return response
 
-        if not body['action'] in VideoController.post_actions:
+        if not body['action'] in ParametersController.post_actions:
             response = {
                 "success": False,
                 "message": f"Action {body['action']} not found"
@@ -57,7 +45,7 @@ class VideoController:
         payload = body['payload']
 
         print("##################", action, payload)
-        response = VideoController.post(action, payload)
+        response = ParametersController.post(action, payload)
         return response
 
     def parseRequestBody(body: bytes):
@@ -74,18 +62,18 @@ class VideoController:
             }
             return response
 
-        if not action in VideoController.get_actions:
+        if not action in ParametersController.get_actions:
             response = {
                 "success": False,
                 "message": f"Action {action} not found"
             }
             return response
 
-        response = VideoController.get(action, request)
+        response = ParametersController.get(action, request)
         return response
 
     def post(action, payload):
-        return VideoController.post_actions[action](payload)
+        return ParametersController.post_actions[action](payload)
 
     def get(action, payload):
-        return VideoController.get_actions[action](payload)
+        return ParametersController.get_actions[action](payload)

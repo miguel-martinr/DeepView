7ad2668602ffff04f720f65933a6a8459ae8e81b
Miguel Martín <alu0101209777@ull.edu.es>
2022-07-01 13:23:31 +0100

    parameters for video can be requested

--- deepcom/controllers/Parameters/get_parameters.py
+++ deepcom/controllers/Parameters/get_parameters.py
@@ -1,9 +1,27 @@
 from django.http import HttpRequest
+from deepcom.apps import DeepcomConfig
+
+from deepcom.services.ParametersService import ParametersService
 
 
 def get_parameters(request: HttpRequest):
+    """
+      Returns parameters linked for specific video. If there are no parameters 
+      linked for that video it returns the default parameters
+    """
+
+    video_name = request.GET.get('video_name')
+
+    if video_name is None:
+        return {
+            "success": False,
+            "message": "No video_name provided"
+        }
+
+    video_path = DeepcomConfig.getVideoPath(video_name)
+    parameters = ParametersService.getParametersForVideo(video_path)
 
     return {
         "success": True,
-        "message": "Testing"
+        "parameters": parameters
     }

3c010e42af05115982b2195bb9b790498aa3ddae
Miguel Martín <alu0101209777@ull.edu.es>
2022-07-01 18:42:23 +0100

    model updated

--- deepcom/models.py
+++ deepcom/models.py
@@ -6,37 +6,43 @@ from deepcom.apps import DeepcomConfig
 from .custom_models.processing_parameters import PreprocessParameters, ProcessParameters
 
 
-class ParticleData(models.Model):
-    x = models.FloatField()
-    y = models.FloatField()
-    radius = models.FloatField()
-    area = models.FloatField()
+# class ParticleData(models.Model):
+#     x = models.FloatField()
+#     y = models.FloatField()
+#     radius = models.FloatField()
+#     area = models.FloatField()
+
+#     class Meta:
+#         abstract = True
+
+
+# class ParticleDataForm(forms.ModelForm):
+#     class Meta:
+#         model = ParticleData
+#         fields = ('x', 'y', 'radius', 'area')
+
+
+# class Frame(models.Model):
+#     particles = models.ArrayField(
+#         model_container=ParticleData,
+#         model_form_class=ParticleDataForm,
+#     )
+
+#     class Meta:
+#         abstract = True
+
+
+# class FrameForm(forms.ModelForm):
+#     class Meta:
+#         model = Frame
+#         fields = ('particles',)
+
+
+class Second(models.Model):
+    data = models.IntegerField(null=False)
 
     class Meta:
         abstract = True
-
-
-class ParticleDataForm(forms.ModelForm):
-    class Meta:
-        model = ParticleData
-        fields = ('x', 'y', 'radius', 'area')
-
-
-class Frame(models.Model):
-    particles = models.ArrayField(
-        model_container=ParticleData,
-        model_form_class=ParticleDataForm,
-    )
-
-    class Meta:
-        abstract = True
-
-
-class FrameForm(forms.ModelForm):
-    class Meta:
-        model = Frame
-        fields = ('particles',)
-
 
 class VideoModel(models.Model):
     _id = models.ObjectIdField()
@@ -45,15 +51,13 @@ class VideoModel(models.Model):
     video_path = models.CharField(max_length=255)
     status = models.CharField(default='UNPROCESSED', max_length=255)
 
-    frames = models.ArrayField(
-        model_container=Frame,
-        model_form_class=FrameForm,
-
+    by_second = models.ArrayField(
+      model_container=Second
     )
 
     objects = models.DjongoManager()
 
-class ProcessingParametersModel(models.Model):
+class ProcessingParametersModel(models.Model):  
   preprocess = models.EmbeddedField(
       model_container=PreprocessParameters,
       null=False,

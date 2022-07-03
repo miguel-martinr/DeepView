31aa9ce0eddb5c3c3fea433e0295cf7ff9afb3a4
Miguel Martín <alu0101209777@ull.edu.es>
2022-07-03 16:59:44 +0100

    chdir added

--- install.py
+++ install.py
@@ -1,5 +1,12 @@
+import os
 from subprocess import Popen
 from time import sleep, time
+
+# get this dir path
+workdir = os.path.dirname(os.path.abspath(__file__))
+
+# set this path as working path
+os.chdir(workdir)
 
 
 log_file = open("log.txt", "w")
--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+import os
 from subprocess import Popen
 from time import sleep, time
 
+
+# get this dir path
+workdir = os.path.dirname(os.path.abspath(__file__))
+
+# set this path as working path
+os.chdir(workdir)
 
 log_file = open("run_log.txt", "w")
 p = Popen(["pipenv", "run", "python", "manage.py", "runserver"], stderr=log_file)
